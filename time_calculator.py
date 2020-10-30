dcc9a4d90d7e3581ab8712119ab241c3c5e0d5ce
Ewa Łobejko <lobejkoewa@gmail.com>
2020-10-30 09:29:11 +0000

    Final version with days

--- time_calculator.py
+++ time_calculator.py
@@ -1,8 +1,9 @@
-def add_time(start, duration):
+def add_time(start, duration, day = None):
 
   start_time, am_pm = start.split() 
   note = ""
-
+  week = ['Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  
   hour = int(start_time.split(':')[0])
   minute = int(start_time.split(':')[1])
   duration_hours = int(duration.split(':')[0])
@@ -13,21 +14,33 @@ def add_time(start, duration):
     duration_hours = duration_hours + 1
     new_minute = new_minute - 60
 
-  days = 0
-  for h in range (duration_hours):
+  days = int(duration_hours / 24)
+  for h in range (duration_hours%24):
     hour = hour + 1
     if hour == 12:
-      
+      if am_pm == "AM":
+        am_pm = "PM"
+      else:
+        am_pm = "AM"
+        days = days + 1
+    if hour > 12:
+      hour = 1
 
-  
+  if days == 1:
+    note = " (next day)"
+  if days > 1:
+    note = " (" + str(days) + " days later)"
 
-  if new_hour >= 12:
-    if am_pm == "AM":
-      am_pm = "PM"
-    else:
-      am_pm = "AM"
-    if new_hour > 12:
-      new_hour = new_hour - 12
+  new_time = str(hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + note
 
-  new_time = str(new_hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + " " + note
+  if day != None:
+    ii = [d.lower() for d in week].index(day.lower())
+    new_ii = ii + int(days % 7) 
+    if new_ii > 6:
+      new_ii = new_ii - 7
+
+    new_day = week[new_ii]
+    new_time = str(hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + ", " + new_day + note
+
+        
   return new_time
