f15f6207069f6d8145d694a78f14fb1ea7e8693b
Ewa Łobejko <lobejkoewa@gmail.com>
2020-10-30 11:19:38 +0000

    Minor changes

--- time_calculator.py
+++ time_calculator.py
@@ -2,14 +2,13 @@ def add_time(start, duration, day = None):
 
   start_time, am_pm = start.split() 
   note = ""
-  week = ['Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
   
   hour = int(start_time.split(':')[0])
   minute = int(start_time.split(':')[1])
   duration_hours = int(duration.split(':')[0])
   duration_minutes =  int(duration.split(':')[1])
 
-  new_minute = int(minute) + int(duration_minutes)
+  new_minute = minute + duration_minutes
   if new_minute > 60:
     duration_hours = duration_hours + 1
     new_minute = new_minute - 60
@@ -34,13 +33,8 @@ def add_time(start, duration, day = None):
   new_time = str(hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + note
 
   if day != None:
+    week = ['Monday','Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     ii = [d.lower() for d in week].index(day.lower())
-    new_ii = ii + int(days % 7) 
-    if new_ii > 6:
-      new_ii = new_ii - 7
+    new_time = str(hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + ", " + week[(ii + days)%7] + note
 
-    new_day = week[new_ii]
-    new_time = str(hour) + ":" + str(new_minute).zfill(2) + " " + am_pm + ", " + new_day + note
-
-        
   return new_time
